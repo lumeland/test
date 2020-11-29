324041c4050fa247dc423c8d564f94b375cac46b
Oscar Otero <oom@oscarotero.com>
2020-11-30 00:25:29 +0100

    test changes

--- test.sh
+++ test.sh
@@ -1,9 +1,8 @@
+# Execute a command
+deno run --unstable -A lume/cli.js --run multi
+
+# Build a site
 deno run --unstable -A lume/cli.js
 DIFF="$(git diff --no-index _expected _site)"
-
-if ["$DIFF" != ""]; then
-  echo 'Test failed' >&2  # write error message to stderr
-  exit 1                  # or exit $test_result
-fi
-
-deno run --unstable -A lume/cli.js --run multi
+echo "Diff:"
+echo "$DIFF"
