bed8f7c2c65256327e146f9f3400019ee37b91e7
Oscar Otero <oom@oscarotero.com>
2020-11-29 15:23:37 +0100

    updated test

--- _config.js
+++ _config.js
@@ -10,9 +10,12 @@ site.script(
   "multi",
   "mkdir newdir && cd newdir && touch newfile && ls -al && cd .. && rm -rf newdir",
 );
+
 site.script(
   "log",
   () => Deno.writeTextFile(site.dest("hello.txt"), "Hello world"),
 );
+
+site.addEventListener("afterBuild", "log")
 
 export default site;
--- test.sh
+++ test.sh
@@ -1,2 +1,3 @@
 deno run --unstable -A lume/cli.js
 git diff --no-index _expected _site > diff
+deno run --unstable -A lume/cli.js --run multi
