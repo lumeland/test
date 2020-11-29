749020dde10bcbba226b709029f1346f1b2db899
Oscar Otero <oom@oscarotero.com>
2020-11-29 16:32:20 +0100

    change test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,9 @@
 deno run --unstable -A lume/cli.js
-git diff --no-index _expected _site > diff
+DIFF="$(git diff --no-index _expected _site)"
+
+if ["$DIFF" != ""]; then
+  echo 'Test failed' >&2  # write error message to stderr
+  exit 1                  # or exit $test_result
+fi
+
 deno run --unstable -A lume/cli.js --run multi
